b0c7c2d43688ffece445a928f851847883730358
atargaryen54-droid <atargaryen54@gmail.com>
2026-02-25 15:12:11 +0300

    creating uuser model, user schema, repository and service along with registration route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from app.api.routes import auth
+
+
 
 app = FastAPI()
+
+app.include_router(auth.router)
+
 
 @app.get("/")
 def health_check():
---
+++ app/schemas/user.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr, Field
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str = Field(
+        ..., 
+        min_length=8, 
+        max_length=100,
+        description="Password must be between 8 and 100 characters")
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+    is_verified: bool
+
+    class Config:
+        from_attributes = True
